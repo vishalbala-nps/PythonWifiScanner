8ab753c8c9cac0091dbf642649cf2d82b43d6f66
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2020-01-16 18:26:27 +0530

    Implemented Vendor Displaying

--- scanner.py
+++ scanner.py
@@ -1,6 +1,14 @@
 import os
 import arpreq
 from prettytable import PrettyTable
+import requests
+
+def getvendor(mac):
+   rval = requests.get("https://api.macvendors.com/"+mac)
+   if rval.status_code != 200:
+      return "Not found"
+   else:
+      return rval.text
 
 if os.geteuid() !=  0:
     print("Please run as root")
@@ -8,8 +16,8 @@ if os.geteuid() !=  0:
 
 sysip = os.popen("hostname -I | awk '{print $1}'").read().strip()
 sysip = sysip.split(".")
-myip = sysip[0]+"."+sysip[1]+"."+sysip[2]+"."
-t = PrettyTable(['id','IP Address', 'MAC Address'])
+mynet = sysip[0]+"."+sysip[1]+"."+sysip[2]+"."
+t = PrettyTable(['id','IP Address', 'MAC Address','Device Vendor'])
 item_no = 0
 
 print("-------------------------")
@@ -19,14 +27,21 @@ print("-------------------------")
 timeout = input("Please enter the ping timeout [default:1.5]") or "1.5"
 print("Scanning Network... (this will take a while)")
 for i in range(1,255):
-  cmd = "ping "+myip+str(i)+" -c 1 -W "+timeout+"> /dev/null"
+  cmd = "ping "+mynet+str(i)+" -c 1 -W "+timeout+"> /dev/null"
   rcode = os.system(cmd)
   if rcode == 0:
     item_no = item_no + 1
-    ip = "192.168.1."+str(i)
-    mac = arpreq.arpreq("192.168.1."+str(i))
+    ip = mynet+str(i)
+    mac = arpreq.arpreq(mynet+str(i))
     if mac == None:
        mac = "Not found"
-    t.add_row([item_no,ip,mac])
+       vendor = "Not found"
+    else:
+       vendor = getvendor(mac)
+    t.add_row([item_no,ip,mac,vendor])
+  if i%10 == 0:
+    print("Scanned IPs do far: "+str(i))
+print("Network scan is completed.")
+print("Devices found:")  
 print(t)
 print("Total number of devices:"+str(item_no))
